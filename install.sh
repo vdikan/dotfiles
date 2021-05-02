a349abd7af05ce38705aecc6dcf2dee6186b0841
Vladimir Dikan <vdikan@vivaldi.net>
2021-05-02 14:17:56 +0300

    dotfiles reduced to terminal settings

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="zshrc tmux.conf stumpwmrc Xresources" # list of files/folders to symlink in homedir
+files="tmux.conf Xresources" # list of files/folders to symlink in homedir
 
 ##########
 
